6f4e35cc5122d9ca827632d378d0af4482ad5ac4
mdycando-dev <mdy.cando@yavirac.edu.ec>
2026-05-31 10:30:58 -0500

    Create area_circulo.py

---
+++ area_circulo.py
@@ -0,0 +1,10 @@
+def calcular_area():
+
+    valor_radio = float(input("Digite el radio del círculo: "))
+
+    constante_pi = 3.1416
+    resultado_area = constante_pi * (valor_radio * valor_radio)
+
+    print("El resultado del área es:", resultado_area)
+
+calcular_area()
